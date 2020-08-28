1b8a2e21e8b135d1f351bd35248e2577525ca7b6
Arati Bhandwalkar <arativbhandwalkar@gmail.com>
2020-08-27 18:38:13 -0700

    connect to heroku

---
+++ client/build/precache-manifest.0281fc1a9deae50e935ab086095bf170.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "00da3f3660cf343e2d45",
+    "url": "/static/css/main.f66d6186.chunk.css"
+  },
+  {
+    "revision": "00da3f3660cf343e2d45",
+    "url": "/static/js/main.8bcc0f2c.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "5352b14d9105df57ac78",
+    "url": "/static/js/2.d3e05863.chunk.js"
+  },
+  {
+    "revision": "fde69b7926f0662c1ed255a9006b55de",
+    "url": "/static/media/public-library.fde69b79.jpg"
+  },
+  {
+    "revision": "2d7300ff4ce2cd2fcbdd7297cdc41796",
+    "url": "/index.html"
+  }
+];
